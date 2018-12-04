fa873b3fb1af62734323cf88414f640495b5ecea
Yonni Tabeling <tabeling.yonni@gmail.com>
2018-12-04 17:53:49 +0100

    update vars

--- src/components/common/TextVars.js
+++ src/components/common/TextVars.js
@@ -5,8 +5,39 @@ const WhatTitle = 'Notre activité';
 const WhatContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inrin laoreet metus, id pulvinar erat. Morbi dignissim nibh sollicitudin.';
 const WhatLegend = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
-
+const LogotypeContent = [
+  {
+    src: './img/logotype.png',
+    alt: 'Logo de Logotype ou logotypage',
+    title: 'Logotype',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in semper nisl, quis feugiat tellus. Pellentesque aliquam eros eu mauris convallis sodales.',
+    key: 0
+  },
+  {
+    src: './img/webdesign.png',
+    alt: 'Logo de web design',
+    title: 'Web design',
+    text: 'Lorem ipsum dolor sit elit. Proin in semper nisl, quis feugiat tellus. Pellentesque aliquam eros eu mauris convallis sodales.',
+    key: 1
+  },
+  {
+    src: './img/devweb.png',
+    alt: 'logo arobase de web développement',
+    title: 'Dev Web',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing consectetur adipiscing elit. Semper nisl, quis feugiat tellus. Pellentesque aliquam eros eu mauris convallis sodales.',
+    key: 2
+  },
+  {
+    src: './img/webapp.png',
+    alt: 'logo d\'application mobile',
+    title: 'Appli mobile',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in semper nisl, feugiat tellus. Pellentesque aliquam eros eu mauris convallis sodales.',
+    key: 3
+  }
+];
 //Export for SectionWho.js
 export {WhoTitle, WhoContent, WhoLegend};
 //Export for SectionWhat.js
 export {WhatTitle, WhatContent, WhatLegend};
+//Export for SectionLogotype
+export {LogotypeContent};
