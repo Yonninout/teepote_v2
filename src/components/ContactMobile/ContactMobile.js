4d568697aff2205ae7275ffb491082d0ba806465
Yonni Tabeling <tabeling.yonni@gmail.com>
2018-12-23 16:54:09 +0100

    test aws ses sdk

--- src/components/ContactMobile/ContactMobile.js
+++ src/components/ContactMobile/ContactMobile.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
 import './ContactMobile.scss';
 
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'eu-west-1'});
+
+// Create sendEmail params
+var params = {
+  Destination: { /* required */
+    CcAddresses: [
+      'yonni.tabeling@teepote.com',
+      /* more items */
+    ],
+    ToAddresses: [
+      'yonni.tabeling@teepote.com',
+      /* more items */
+    ]
+  },
+  Message: { /* required */
+    Body: { /* required */
+      Html: {
+       Charset: "UTF-8",
+       Data: "HTML_FORMAT_BODY"
+      },
+      Text: {
+       Charset: "UTF-8",
+       Data: "TEXT_FORMAT_BODY"
+      }
+     },
+     Subject: {
+      Charset: 'UTF-8',
+      Data: 'Test email'
+     }
+    },
+  Source: 'SENDER_EMAIL_ADDRESS', /* required */
+  ReplyToAddresses: [
+      'yonni.tabeling@teepote.com',
+    /* more items */
+  ],
+};
+
+var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+// Handle promise's fulfilled/rejected state
+ssendPromise.then(
+  function(data) {
+    console.log(data.MessageId);
+  }).catch(
+    function(err) {
+    console.error(err, err.stack);
+  });
+
+
 class ContactMobile extends Component {
   constructor(props) {
   super(props);
@@ -36,8 +87,23 @@ handleInputChange(event) {
   // console.log(this.state);
   }
 
+  // Load the AWS SDK for Node.js
+
+
+  // Create the promise and SES service object
+  var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+  // Handle promise's fulfilled/rejected states
+  sendPromise.then(
+    function(data) {
+      console.log(data.MessageId);
+    }).catch(
+      function(err) {
+      console.error(err, err.stack);
+    });
 
 render() {
+
     return (
       <div className="ContactMobile" onClick={this.props.onClick}>
         <div className="Intro">
