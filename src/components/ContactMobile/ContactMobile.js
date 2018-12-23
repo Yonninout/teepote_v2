7ca7f3c75b9ae3a2d91a2aa22c5a4b4038fe050b
Yonni Tabeling <tabeling.yonni@gmail.com>
2018-12-23 03:55:33 +0100

    create form structure

--- src/components/ContactMobile/ContactMobile.js
+++ src/components/ContactMobile/ContactMobile.js
@@ -2,10 +2,114 @@ import React, { Component } from 'react';
 import './ContactMobile.scss';
 
 class ContactMobile extends Component {
-  render() {
+  constructor(props) {
+  super(props);
+  this.state = {
+    lastName: '',
+    firstName: '',
+    society: '',
+    email: '',
+    phoneNumber: '',
+    object: '',
+    message: '',
+
+  };
+  this.handleInputChange = this.handleInputChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
+}
+
+handleSubmit(event) {
+  console.log(this.state);
+   event.preventDefault();
+   return false;
+}
+
+handleInputChange(event) {
+  const target = event.target;
+  const value = target.value;
+  const name = target.name;
+
+  this.setState({
+    [name]: value
+  });
+  // console.log(this.state);
+  }
+
+
+render() {
     return (
       <div className="ContactMobile" onClick={this.props.onClick}>
+        <div className="Intro">
+          <div className="Head">
+            Contactez nous
+          </div>
+          <div className="ContactText">Téléphones : </div>
+          <div className="Contact">CV : 07 82 69 56 79</div>
+          <div className="Contact">YT : 06 14 72 20 83</div>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Nom * :
+            <input
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Prénom * :
+            <input
+              name="firstName"
+              type="text"
+              value={this.state.firstName}
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            email * :
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInputChange} />
+            </label>
+          <label>
+            Téléphone :
+            <input
+              name="phoneNumber"
+              type="tel"
+              value={this.state.phoneNumber}
+              onChange={this.handleInputChange} />
+            </label>
+          <label>
+            Societé :
+            <input
+              name="society"
+              type="text"
+              value={this.state.society}
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Objet * :
+            <input
+              name="object"
+              type="text"
+              value={this.state.object}
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Message * :
+            <textarea
+              name="message"
+              type="text"
+              rows = {7}
+              value={this.state.message}
+              onChange={this.handleInputChange} />
+          </label>
+          <p>* : Champs Obligatoires</p>
 
+          <input type="submit" value="Envoyer"/>
+        </form>
       </div>
     );
   }
